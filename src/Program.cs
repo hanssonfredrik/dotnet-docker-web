8dfee0acd5bd8f216b2810be1f90d7c14c00d897
Fredrik Hansson <fredrikha@gmail.com>
2023-10-17 09:19:53 +0200

    Disables reloadOnChange

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-// builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
 
 var app = builder.Build();
 
